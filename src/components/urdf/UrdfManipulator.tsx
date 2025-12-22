661d011598a8dc3a222d0375f72f392740091112
alfie <alfie2002a@gmail.com>
2025-12-22 23:29:33 +0800

    [fix] urdf path error

--- src/components/urdf/UrdfManipulator.tsx
+++ src/components/urdf/UrdfManipulator.tsx
@@ -39,13 +39,13 @@ function URDFModel({ path, modelRef, onReady }: {
       }
       modelRef.current = null;
     };
-    
+
   }, [path, scene, modelRef, onReady]);
 
   return null;
 }
 
-export default function URDFManipulator({ modelRef, onReady }: { 
+export default function URDFManipulator({ modelRef, onReady }: {
   modelRef: React.RefObject<URDFRobot | null>;
   onReady: () => void;
 }) {
@@ -75,7 +75,11 @@ export default function URDFManipulator({ modelRef, onReady }: {
         intensity={0.8}
       />
 
-      <URDFModel path="/urdf/piper_description/urdf/piper_description.URDF" modelRef={modelRef} onReady={onReady} />
+      <URDFModel
+        path={`${import.meta.env.BASE_URL}urdf/piper_description/urdf/piper_description.URDF`}
+        modelRef={modelRef}
+        onReady={onReady}
+      />
 
       <OrbitControls enableDamping dampingFactor={0.05} />
     </Canvas>
